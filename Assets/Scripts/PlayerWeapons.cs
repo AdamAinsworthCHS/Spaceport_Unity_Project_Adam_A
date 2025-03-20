be9338ada87ae28457a636e1afd65afd379db6cb
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-03-20 10:32:03 -0700

    improved ship weapon systems

--- Assets/Scripts/PlayerWeapons.cs
+++ Assets/Scripts/PlayerWeapons.cs
@@ -5,21 +5,22 @@ public class PlayerWeapons : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     public GameObject projectile;
     public GameObject player;
-    private Vector3 offset = new Vector3(0, -1.5f, 1);
+    private Vector3 offset = new Vector3(0, 0, 0);
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey("space"))
         {
-            GameObject clone;
-            clone = Instantiate(projectile, player.transform.position + offset, player.transform.rotation);
-            transform.Translate(Vector3.forward * Time.deltaTime * -10);
+            {
+                GameObject clone;
+                clone = Instantiate(projectile, player.transform.position + offset, player.transform.rotation);
+            }
         }
     }
 }
--- Assets/Scripts/ProjectileBehavior.cs
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -5,7 +5,7 @@ public class ProjectileBehavior : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.Rotate(90, 0, 0);
     }
 
     public float speed = 40.0f;
@@ -13,6 +13,6 @@ public class ProjectileBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * (speed * -1));
     }
 }
