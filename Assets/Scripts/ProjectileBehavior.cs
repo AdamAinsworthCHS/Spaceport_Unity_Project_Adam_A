2e4f5d152c91d72506dffd0fb3613cfa671ca3bb
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-03-20 10:46:51 -0700

    made projectiles despawn if they get too far away

--- Assets/Scripts/ProjectileBehavior.cs
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class ProjectileBehavior : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public GameObject player;
+    
     void Start()
     {
         transform.Rotate(90, 0, 0);
+        player = GameObject.Find("Player_Ship");
     }
 
     public float speed = 40.0f;
@@ -14,5 +18,10 @@ public class ProjectileBehavior : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * (speed * -1));
+        
+        if (transform.position.x > player.transform.position.x + 100 || transform.position.y > player.transform.position.y + 100 || transform.position.z > player.transform.position.z + 100)
+        {
+            Destroy(gameObject);
+        }
     }
 }
