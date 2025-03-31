4a9725e6e5d8627ca039664696df2062c729bc8c
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-03-31 11:28:15 -0700

    player weapons can now kill enemies

--- Assets/Scripts/Enemy_Behavior.cs
+++ Assets/Scripts/Enemy_Behavior.cs
@@ -11,6 +11,10 @@ public class Enemy_Behavior : MonoBehaviour
     }
 
     public float speed = 2.0f;
+    public float health = 100;
+    public Material hurtMaterial;
+    public Material shipMaterial;
+    private float timeStamp = 0;
 
     // Update is called once per frame
     void Update()
@@ -18,6 +22,23 @@ public class Enemy_Behavior : MonoBehaviour
         if (transform.position.x > player.transform.position.x + 10 || transform.position.y > player.transform.position.y + 10 || transform.position.z > player.transform.position.z + 10)
         {
             transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (timeStamp <= Time.time)
+        {
+            GetComponent<Renderer>().material = shipMaterial;
+            timeStamp = Time.time + 0.1f;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player Projectile") == true)
+        {
+            health--;
+            GetComponent<Renderer>().material = hurtMaterial;
         }
     }
 }
