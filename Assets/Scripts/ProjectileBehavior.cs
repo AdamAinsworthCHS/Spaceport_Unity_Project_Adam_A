664bcf6bab2a2c9445c494602860b7f0072dba89
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-04-02 11:52:28 -0700

    fixed a bug where projectiles wouldn't despawn

--- Assets/Scripts/ProjectileBehavior.cs
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -19,7 +19,7 @@ public class ProjectileBehavior : MonoBehaviour
     {
         transform.Translate(Vector3.forward * Time.deltaTime * (speed * -1));
         
-        if (transform.position.x > player.transform.position.x + 100 || transform.position.y > player.transform.position.y + 100 || transform.position.z > player.transform.position.z + 100)
+        if (transform.position.x > player.transform.position.x + 100 || transform.position.y > player.transform.position.y + 100 || transform.position.z > player.transform.position.z + 100 || transform.position.x < player.transform.position.x - 100 || transform.position.y < player.transform.position.y - 100 || transform.position.z < player.transform.position.z - 100)
         {
             Destroy(gameObject);
         }
